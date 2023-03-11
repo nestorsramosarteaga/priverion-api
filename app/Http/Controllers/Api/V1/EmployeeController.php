c75006a880a3f81a8ea1db04b7f1d857627c83ea
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 02:08:59 -0500

    Update Employee Controller

--- app/Http/Controllers/Api/V1/EmployeeController.php
+++ app/Http/Controllers/Api/V1/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\EmployeeResource;
 use Illuminate\Http\Request;
 use App\Models\Employee;
 
@@ -10,26 +11,60 @@ class EmployeeController extends Controller
 {
     public function index()
     {
-        //
+        return EmployeeResource::collection(Employee::latest()->paginate());
     }
 
     public function store(Request $request)
     {
-        //
+        $fields = $request->all();
+
+        try {
+            $employee = new Employee($fields);
+            $employee->save();
+
+            return response()->json([
+                'message'=>'Employee Created Successfully!!',
+                'data' => $employee
+            ]);
+        
+        } catch (\Exception $e){
+            return response()->json([
+                'message'=>'Something went wrong while creating an employee!'
+            ], 500);
+        }
     }
 
     public function show(Employee $employee)
     {
-        //
+        return new EmployeeResource($employee);
     }
 
     public function update(Request $request, Employee $employee)
     {
-        //
+        try {
+            $employee->update($request->input());
+
+            return response()->json([
+                'message'=>'Employee Updated Successfully!!',
+                'data' => $employee
+            ]);
+
+        } catch (\Exception $e){
+            return response()->json([
+                'message'=>'Something went wrong while updating an employee!'
+            ], 500);
+        }
     }
 
     public function destroy(Employee $employee)
     {
-        //
+        if($employee->delete()) {
+            return response()->json([
+                'message' => 'The employee was deleted successfully'
+            ], 204);
+        }
+        return response()->json([
+            'message' => 'Not Found'
+        ], 404);
     }
 }
