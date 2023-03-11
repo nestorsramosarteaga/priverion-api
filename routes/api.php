c89c2a75884ca6dab11e22cd09652faa011b8fa6
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 03:11:43 -0500

    Add Routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\V1\EmployeeController;
+use App\Http\Controllers\Api\V1\AuthController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => '/v1',
+    'as' => 'v1',
+  ], function () {
+
+    Route::post('auth', [AuthController::class, 'auth']);
+
+    Route::apiResource('employees', EmployeeController::class)
+        ->middleware('auth:sanctum');
 });
