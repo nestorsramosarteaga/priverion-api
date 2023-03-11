6d20754b9d3ec6f728e4b0efcf898f5129221a25
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 01:37:09 -0500

    Move Employee Controller to Api/V1 folder

--- app/Http/Controllers/Api/V1/EmployeeController.php
+++ app/Http/Controllers/Api/V1/EmployeeController.php
@@ -1,47 +1,33 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
-use App\Models\Employee;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Employee $employee)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Employee $employee)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Employee $employee)
     {
         //
