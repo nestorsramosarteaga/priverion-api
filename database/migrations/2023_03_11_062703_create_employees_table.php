a6510789623d26bb8914358d3c5f5e5911d90f2a
Nestor Ramos <nestorsramosarteaga@gmail.com>
2023-03-11 01:37:39 -0500

    Update employees migration

--- database/migrations/2023_03_11_062703_create_employees_table.php
+++ database/migrations/2023_03_11_062703_create_employees_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->string('email', 255);
+            $table->string('phone', 20);
             $table->timestamps();
         });
     }
